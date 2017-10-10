b2034731f7191f31359ef16d3267233891eb0fca
wym613 <31758925+wym613@users.noreply.github.com>
2017-10-10 00:26:37 -0400

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -6,14 +6,6 @@
 //  Copyright © 2017 Yimeng Wang. All rights reserved.
 //
 
-//
-//  AppDelegate.swift
-//  FoodTracker
-//
-//  Created by Jane Appleseed on 10/17/16.
-//  Copyright © 2016 Apple Inc. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
